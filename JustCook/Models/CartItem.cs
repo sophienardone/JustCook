c435d6169b10090dc223ae5c923cabbad82adb67
smath <smathewslynch@gmail.com>
2025-04-22 16:09:17 +0100

    Created CartItem model and Cart class

---
+++ JustCook/Models/CartItem.cs
@@ -0,0 +1,17 @@
+﻿namespace JustCook.Models
+{
+    public class CartItem
+    {
+        public int CartItemId { get; set; }
+        public int BookId { get; set; }
+        public CookBook? Book { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+    public class Cart
+    {
+        public List<CartItem> Items { get; set; } = new List<CartItem>();
+
+        public decimal Total => Items.Sum(item => item.UnitPrice * item.Quantity);
+    }
+}
