432b6937b726f4a8c0f87eb11f53b03705bd5be0
smath <smathewslynch@gmail.com>
2025-04-29 06:37:25 +0100

    Added Map Controller to give google map its own page

---
+++ JustCook/Controllers/MapController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JustCook.Controllers
+{
+    public class MapController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
